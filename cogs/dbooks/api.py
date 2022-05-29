7829efcaca1a1d209f6c278fc99c4be4f0527b4f
chawkk6404 <chawk_k@outlook.com>
2022-05-29 11:46:56 -0700

    Add bot typing

--- cogs/dbooks/api.py
+++ cogs/dbooks/api.py
@@ -1,24 +1,21 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 import discord
 
-import aiohttp
+if TYPE_CHECKING:
+    from bot import BookShelf
 
 
 MISSING = discord.utils.MISSING
 
 
 class DBooksClient:
-    def __init__(self):
-        self.session: aiohttp.ClientSession = MISSING
-
-    async def cog_load(self) -> None:
-        self.session = aiohttp.ClientSession(raise_for_status=True)
-
-    async def cog_unload(self) -> None:
-        if self.session and not self.session.closed:
-            await self.session.close()
+    bot: BookShelf
 
     async def search(self, query: str, *, result: int = 0) -> dict:
-        async with self.session.get(f'https://www.dbooks.org/api/search/{query}') as resp:
+        async with self.bot.session.get(f'https://www.dbooks.org/api/search/{query}') as resp:
             data = await resp.json()
         return data['books'][result]
 
