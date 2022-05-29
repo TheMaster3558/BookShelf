29ea9758dd9a69dee4752b7268f723a183e8af50
chawkk6404 <chawk_k@outlook.com>
2022-05-29 16:38:33 -0700

    Add useful utils

--- bot.py
+++ bot.py
@@ -8,8 +8,31 @@ from discord.ext import commands
 MISSING = discord.utils.MISSING
 
 
+def setup_logging():
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(filename='logs/discord.log', encoding='utf-8', mode='w')
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    logger.addHandler(handler)
+
+    logger = logging.getLogger('asyncio')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(filename='logs/asyncio.log', encoding='utf-8', mode='w')
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    logger.addHandler(handler)
+
+
+def cythonize():
+    import os
+    from Cython.Build import cythonize
+
+    for file in os.listdir('./cython/'):
+        if file.endswith('.pyx'):
+            cythonize(file)
+
+
 class BookShelf(commands.Bot):
-    __version__ = '1.0.0ag'
+    __version__ = '1.0.0a'
 
     initial_extensions = (
         'cogs.share',
@@ -25,19 +48,9 @@ class BookShelf(commands.Bot):
 
         super().__init__('bk ', intents=intents)
 
-        logger = logging.getLogger('discord')
-        logger.setLevel(logging.DEBUG)
-        handler = logging.FileHandler(filename='logs/discord.log', encoding='utf-8', mode='w')
-        handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-        logger.addHandler(handler)
-
-        logger = logging.getLogger('asyncio')
-        logger.setLevel(logging.DEBUG)
-        handler = logging.FileHandler(filename='logs/asyncio.log', encoding='utf-8', mode='w')
-        handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-        logger.addHandler(handler)
-
         self.session: aiohttp.ClientSession = MISSING
+
+        setup_logging()
 
     async def setup_hook(self) -> None:
         self.session = aiohttp.ClientSession()
