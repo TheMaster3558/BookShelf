e545ffaba3949b20cabfdb682841214d5ca26673
chawkk6404 <chawk_k@outlook.com>
2022-07-03 12:07:20 -0700

    Change style to paragraph

--- cogs/python/views.py
+++ cogs/python/views.py
@@ -4,7 +4,8 @@ import discord
 class CodeModal(discord.ui.Modal, title='Eval'):
     code: discord.ui.TextInput = discord.ui.TextInput(
         label='Python Code',
-        placeholder='print(\'Hello world\')'
+        placeholder='print(\'Hello world\')',
+        style=discord.TextStyle.paragraph
     )
 
     async def on_submit(self, interaction: discord.Interaction) -> None:
